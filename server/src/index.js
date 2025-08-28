ab4cba42e7b9c48f7289766102962a58faa38167
Swarnavo2003 <swarnavo2020@gmail.com>
2025-08-28 12:03:53 +0530

    Auth Routes Tested

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config({ path: "./.env", quiet: true });
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import connectDB from "./config/db.config.js";
 import { errorHandler } from "./utils/error-handler.js";
+
 import authRouter from "./routes/auth.routes.js";
-dotenv.config({ path: "./.env", quiet: true });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +15,14 @@ const PORT = process.env.PORT || 3000;
 // default middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  })
+);
 
 // api
 app.use("/api/v1/auth", authRouter);
